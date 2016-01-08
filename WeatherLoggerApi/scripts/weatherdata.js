8aac2498f5c6dda1cfa757bfa22d8797debd7bd2
Matt Miller <mattmiller85@gmail.com>
2016-01-08 10:34:10 -0500

    Add web page

---
+++ WeatherLoggerApi/scripts/weatherdata.js
@@ -0,0 +1,22 @@
+﻿var viewModel = {
+    Temperature: ko.observable(),
+    Humidity: ko.observable()
+}
+$(document).ready(function () {
+    ko.applyBindings(viewModel, $("#latest-weather")[0]);
+    getLatestWeatherEntry();
+    setInterval(function () {
+        getLatestWeatherEntry();
+    }, 5000);
+
+});
+
+function getLatestWeatherEntry() {
+    $.getJSON("api/weather/1", function (response) {
+        if (!response || response.length === 0)
+            return;
+        var latest = response[0];
+        viewModel.Temperature(latest.Temperature);
+        viewModel.Humidity(latest.Humidity);
+    });
+}
