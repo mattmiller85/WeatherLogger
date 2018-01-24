cb267f66bd997fb91299924d18e8c481d4ff1c05
mattmiller85 <mattmiller85@gmail.com>
2018-01-24 09:52:08 -0500

    Bootstrap 4 and timestamp for the last recorded date

--- WeatherLoggerApi/scripts/weatherdata.js
+++ WeatherLoggerApi/scripts/weatherdata.js
@@ -1,22 +1,41 @@
-﻿var viewModel = {
-    Temperature: ko.observable(),
-    Humidity: ko.observable()
-}
-$(document).ready(function () {
-    ko.applyBindings(viewModel, $("#latest-weather")[0]);
-    getLatestWeatherEntry();
-    setInterval(function () {
-        getLatestWeatherEntry();
-    }, 30000);
+﻿//@ts-check
 
-});
+const viewModel = {
+    Temperature: ko.observable(""),
+    Humidity: ko.observable(""),
+    TimeStamp: ko.observable(""),
+    RelativeDate: ko.observable("")
+};
+
+ko.applyBindings(viewModel, document.querySelector("#latest-weather"));
+
+showLoading();
+
+getLatestWeatherEntry();
+
+setInterval(function () {
+    getLatestWeatherEntry();
+}, 30000);
+
+function showLoading() {
+    viewModel.Temperature("Loading...");
+    viewModel.Humidity("Loading...");
+    viewModel.TimeStamp("Loading...");
+    viewModel.RelativeDate("Refreshing");
+}
 
 function getLatestWeatherEntry() {
-    $.getJSON("api/weather/1", function (response) {
-        if (!response || response.length === 0)
+    showLoading();
+    fetch("api/weather/1").then(response => {
+        if (!response)
             return;
-        var latest = response[0];
-        viewModel.Temperature(latest.Temperature);
-        viewModel.Humidity(latest.Humidity);
+        response.json().then(resp => {
+            let latest = resp[0];
+            viewModel.Temperature(latest.Temperature);
+            viewModel.Humidity(latest.Humidity);
+            viewModel.TimeStamp(latest.TimeStamp);
+            //@ts-ignore
+            viewModel.RelativeDate(moment(latest.TimeStamp).fromNow())    
+        });
     });
 }
